72929927c448f5719fd559ee683b3f8dc1a8ba0b
GrenManSK <melnicek.richard@gmail.com>
2023-06-15 11:45:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages
 from curses_builder import VERSION, AUTHOR
 
 setup(
-    name ='curses_builder' ,
+    name="curses_builder",
     version=VERSION,
-    description='curses_builder',
+    description="curses_builder",
     author=AUTHOR,
-    install_requires=['windows-curses'],
-    packages=find_packages(exclude=('tests*', 'testing*')),
-    entry_points={}
+    install_requires=["windows-curses", "Levenshtein"],
+    packages=find_packages(exclude=("tests*", "testing*")),
+    entry_points={},
 )
